cd6d8e9bfced72a64f87553ea195093f0ce8bd4d
Fábio Souto <fabiomsouto@gmail.com>
2012-06-15 17:13:02 +0100

    First commit

---
+++ lang/pt/local_digitalis.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 2012, Digitalis Informatica. All rights reserved. Distribuicao e Gestao de Informatica, Lda. Estrada de Paco de Arcos
+ * num.9 - Piso -1 2780-666 Paco de Arcos Telefone: (351) 21 4408990 Fax: (351) 21 4408999 http://www.digitalis.pt
+ */
+
+/**
+ * Web service template plugin related strings
+ * @package   local_digitalis
+ * @copyright 2012 Digitalis Informática
+ * @author    Fábio Souto 
+ */
+
+$string['pluginname'] = 'Extensões de webservice Digitalis';
---
+++ version.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 2012, Digitalis Informatica. All rights reserved. Distribuicao e Gestao de Informatica, Lda. Estrada de Paco de Arcos
+ * num.9 - Piso -1 2780-666 Paco de Arcos Telefone: (351) 21 4408990 Fax: (351) 21 4408999 http://www.digitalis.pt
+ */
+
+$plugin->requires = 2012061200;
+$plugin->version  = 2012061500;   
+$plugin->cron     = 0;
+$plugin->release  = '1.0 (Build: 2012061500)';
+$plugin->maturity = MATURITY_STABLE;
